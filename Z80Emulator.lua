2fd0519ee384317362de3a1ee041d4225e3b78fc
HexBlues <hifreeblues@hotmail.com>
2017-08-08 03:30:58 +0800

    change Register from java class to lua table

--- Z80Emulator.lua
+++ Z80Emulator.lua
@@ -499,13 +499,292 @@ function Z80:inport(int address)
 
 end
 
+function Z80:inport(address)
+	return inport(address.get());
+end
+
 
 -- 输出到 I/O
 function Z80:outport(int address, int value)
 
 end
 
+function Z80:outport(address, value)
+	local typeAddress, typeValue = type(address), type(value)
+	if typeAddress == "table" and typeValue == "number" then
+		Z80:outport(address.get(), value);
+	elseif typeAddress == "number" and typeValue == "table" then
+		Z80:outport(address, value.get());
+	elseif typeAddress == "table" and typeValue == "table" then
+		Z80:outport(address.get(), value.get());
+	end
+end
 
+-- 子程序
+function Z80:subroutine(address)
+
+end
+
+-- 无符号数变为符号数
+function Z80:toSigned(value)
+	return (value & 0xff);
+end
+
+-- 获取R的值
+function Z80:getR()
+	Z80.rnd = Z80.rnd * 8197 + 1;
+	return (Z80.rnd >>> 25) & 0xff;
+end
+
+-- 改变进位标志（8位加法）
+function Z80:setCy8(acc)
+	return (acc & 0x00000100) != 0 ? MASK_CY: 0;
+end
+
+-- 改变进位标志（16位加法）
+function Z80:setCy16(int acc)
+	return (acc & 0x00010000) != 0 ? MASK_CY: 0;
+end
+
+-- 改变进位标志（减法）
+function Z80:setCyS(int acc)
+	return (acc & 0x80000000) != 0 ? MASK_CY: 0;
+end
+
+-- 改变奇偶校验/溢出标志（奇偶校验）
+function Z80:setP(accr)
+	local typeAccr = type(accr)
+	if typeAccr == "number" then 
+		return parity[acc & 0xff];
+	elseif typeAccr == "table" then
+		return parity[r.get() & 0xff];
+	end
+end
+
+-- 改变奇偶校验/溢出标志（8位加法）
+function Z80:setV8(acc, x, y)
+	local typeX = type(x)
+	if typeX == "number" then
+		return (((x ^ y) & 0x80) != 0 ? 0: (((x ^ acc) & 0x80) != 0 ? MASK_PV: 0));
+	elseif typeX == "table" then
+		local _x = x.get();
+		return (((_x ^ y) & 0x80) != 0 ? 0: (((_x ^ acc) & 0x80) != 0 ? MASK_PV: 0));
+	end
+end
+
+-- 改变奇偶校验/溢出标志（16位加法）
+function Z80:setV16(acc, x, y)
+	local _x = x.get();
+	local _y = y.get();
+	return (((_x ^ _y) & 0x8000) != 0 ? 0: (((_x ^ acc) & 0x8000) != 0 ? MASK_PV: 0));
+end
+
+-- 改变奇偶校验/溢出标志（8位减法）
+function Z80:setV8S(acc, x, y)
+	local typeX, typeY = type(x), type(y)
+	if typeX == "number" and typeY == "number" then
+		return (((x ^ y) & 0x80) != 0 ? (((x ^ acc) & 0x80) != 0 ? MASK_PV: 0): 0);
+	elseif typeX == "table" and typeY == "number" then
+		local _x = x.get();
+		return (((_x ^ y) & 0x80) != 0 ? (((_x ^ acc) & 0x80) != 0 ? MASK_PV: 0): 0);
+	elseif typeX == "number" and typeY == "table" then
+		return (((x ^ y.get()) & 0x80) != 0 ? (((x ^ acc) & 0x80) != 0 ? MASK_PV: 0): 0);
+	end	
+end
+
+-- 改变奇偶校验/溢出标志（16位减法）
+function Z80:setV16S(acc, x, y)
+	local _x = x.get();
+	return (((_x ^ y.get()) & 0x80) != 0 ? (((_x ^ acc) & 0x80) != 0 ? MASK_PV: 0): 0);
+end
+
+-- 更改半进位标志（8位加法）(Register8 x, int y, int cy)
+function Z80:setHC8(x, y, cy)
+	local typeX = type(x) 
+	if cy ~= nil then
+		return ((x.get() & 0x0f) + (y & 0x0f) + cy) & 0x10;
+	elseif cy == nil and typeX == "number" then
+		return ((x & 0x0f) + (y & 0x0f)) & 0x10;
+	elseif cy == nil and typeX == "table" then  
+		return ((x.get() & 0x0f) + (y & 0x0f)) & 0x10;
+	end
+end
+
+
+-- 更改半进位标志（8位减法）(Register8 x, int y, int cy)
+function Z80:setHC8S(x, y, cy)
+	local typeX, typeY = type(x), type(y)
+	if cy ~= nil and typeX == "table" and typeY == "number" then
+		return ((x.get() & 0x0f) - (y & 0x0f) - cy) & 0x10;
+	elseif cy == nil and typeX == "number" and typeY == "number" then
+		return ((x & 0x0f) - (y & 0x0f)) & 0x10;
+	elseif cy == nil and typeX == "table" and typeY == "number" then
+		return ((x.get() & 0x0f) - (y & 0x0f)) & 0x10;
+	elseif cy == nil and typeX == "number" and typeY == "table" then
+		return ((x & 0x0f) - (y.get() & 0x0f)) & 0x10;
+	end 	
+end
+
+-- 更改半进位标志（16位加法）(Register16 x, Register16 y, int cy)
+function Z80:setHC16(x, y, cy) 
+	if cy ~= nil then
+		return ((((x.get() & 0x0fff) + (y.get() & 0x0fff) + cy) & 0x1000) != 0 and {MASK_HC} or {0})[1];
+	elseif cy == nil then
+		return ((((x.get() & 0x0fff) + (y.get() & 0x0fff)) & 0x1000) != 0 and {MASK_HC} or {0})[1];
+	end
+end
+
+-- 更改半进位标志（16位减法）(Register16 x, Register16 y, int cy)
+function Z80:setHC16S(x, y, cy)
+	return ((((x.get() & 0x0fff) - (y.get() & 0x0fff) - cy) & 0x1000) != 0 and {MASK_HC} or {0})[1];
+end
+
+-- 改变零标志位（8位）int setZ8(int acc)
+function Z80:setZ8(accr)
+	local typeAccr == type(accr)
+	if typeAccr == "number" then
+		return (acc & 0xff) != 0 ? 0: MASK_Z;
+	elseif typeAccr == "table" then
+		return (r.get() & 0xff) != 0 ? 0: MASK_Z;
+	end
+end
+
+-- 改变零标志位（16位）
+function Z80:setZ16(acc)
+	return (acc & 0xffff) != 0 ? 0: MASK_Z;
+end
+
+-- 更改符号标志（8位）
+function Z80:setS8(accr)
+	local typeAccr == type(accr)
+	if typeAccr == "number" then
+		return (acc & 0x80) != 0 ? MASK_S: 0;
+	elseif typeAccr == "table" then
+		return (((r.get() & 0x80) != 0) and {MASK_S} or {0})[1];
+	end
+end
+
+-- 更改符号标志（16位）(int acc)
+function Z80:setS16(acc)
+	return ((acc & 0x8000) != 0 ? {MASK_S} or {0})[1];
+end
+
+--[[	adc imm
+		adc (HL)
+		adc (IX+d)
+		adc (IY+d)
+--]]  
+function Z80:adc8(n)
+	local a,f,pc = Z80.a, Z80.f, Z80.pc
+	local acc = a.get() + n + f.cy();
+	f.set(Z80:setCy8(acc) | Z80:setV8(acc, a, n) | Z80:setHC8(a, n, f.cy()) | Z80:setZ8(acc) | Z80:setS8(acc));
+	a.set(acc);
+	pc += length;
+end
+
+--[[
+		adc B
+		adc C
+		adc D
+		adc E
+		adc H
+		adc L
+		adc A
+		adc IXH
+		adc IXL
+		adc IYH
+		adc IYL
+--]]
+function Z80:adc8(r)
+	
+end
+
+-- 执行 int execute(int execute_states)
+function Z80:execute(execute_state)
+	Z80.restStates = execute_states;
+	executeStates = Z80.restStates
+
+	if (Z80.hlt) then 
+		if (Z80.trace) then
+			Z80:disassemble();
+		end
+		Z80.restStates = 0;
+		return 0
+	end
+
+	while (Z80.restStates > 0) do
+		if (Z80.trace) then
+			Z80:disassemble();
+		end
+
+		local f1 == Z80:fetchXX()
+
+		if 	   f1 == 0x00 then nop();		-- nop 
+		elseif f1 == 0x01 then ld16(bc, imm16());		-- ld BC, mn
+		elseif f1 == 0x02 then st8(bc, a);		-- ld (BC), A 
+		elseif f1 == 0x03 then inc16(bc);		-- inc BC
+		elseif f1 == 0x04 then inc8_r(b);		-- inc B 
+		elseif f1 == 0x05 then dec8_r(b);		-- dec B
+		elseif f1 == 0x06 then ld8(b, imm8());		-- ld B, n
+		elseif f1 == 0x07 then rlca();		-- rlca
+
+		elseif f1 == 0x08 then ex_r(af, af_d);		-- ex AF, AF'
+		elseif f1 == 0x09 then add16(hl, bc);		-- add HL, BC
+		elseif f1 == 0x0a then ld8(a, mem8(bc));		--ld A, (BC)
+		elseif f1 == 0x0b then dec16(bc);		-- dec BC
+		elseif f1 == 0x0c then inc8_r(c);		-- inc C
+        elseif f1 == 0x0d then dec8_r(c);		-- dec C
+		elseif f1 == 0x0e then ld8(c, imm8());		-- ld C, n
+		elseif f1 == 0x0f then rrca();		-- rrca
+
+		elseif f1 == 0x10 then djnz(dis());		-- djnz e
+		elseif f1 == 0x11 then ld16(de, imm16());		-- ld DE, mn
+		elseif f1 == 0x12 then st8(de, a);		-- ld (DE), A
+		elseif f1 == 0x13 then inc16(de);		-- inc DE
+		elseif f1 == 0x14 then inc8_r(d);		-- inc D
+		elseif f1 == 0x15 then dec8_r(d);		-- dec D
+		elseif f1 == 0x16 then ld8(d, imm8());		-- ld D, n
+		elseif f1 == 0x17 then rla();		-- rla
+	
+		elseif f1 == 0x18 then jr(1, dis());		-- jr e
+		elseif f1 == 0x19 then add16(hl, de);		-- add HL, DE
+		elseif f1 == 0x1a then ld8(a, mem8(de));		-- ld A, (DE)
+		elseif f1 == 0x1b then dec16(de);		-- dec DE
+		elseif f1 == 0x1c then inc8_r(e);		-- inc E
+        elseif f1 == 0x1d then dec8_r(e);		-- dec E
+		elseif f1 == 0x1e then ld8(e, imm8());		-- ld E, n
+		elseif f1 == 0x1f then rra();		-- rra
+
+		
+		elseif f1 == 0xc8 then ret(f.z());		-- ret Z
+		elseif f1 == 0xc9 then ret(1);		-- ret
+		elseif f1 == 0xca then jp(f.z(), imm16());		-- jp Z, mn		
+		elseif f1 == 0xcb then
+			Z80.pc = Z80.pc + 1
+			local f2 = fetchCBXX()
+			if     f2 == 0x00 then rlc_r(b);		-- rlc B
+			elseif f2 == 0x01 then rlc_r(c);		-- rlc C
+			elseif f2 == 0x02 then rlc_r(d);		-- rlc D
+			
+			
+			end
+		elseif f1 == 0xcc then call(f.z(), imm16());		-- call Z, mn
+		elseif f1 == 0xcd then call(1, imm16());		-- call mn
+		elseif f1 == 0xce then adc8(imm8());		-- adc n
+		elseif f1 == 0xcf then rst(0x08);		-- rst 08H
+		
+
+		elseif f1 == 0xfe then cp(imm8());		-- cp n 
+		elseif f1 == 0xff then rst(0x38);		-- rst 38H
+		end
+		
+		Z80.restStates = Z80.restStates - Z80.states;
+
+	end	
+
+	return 0
+
+end 
 
 
 
